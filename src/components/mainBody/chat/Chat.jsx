b70cec1e0c5ea154d6c8abc24ceaab0a63148994
snyder-io <tushar.whitecollar@gmail.com>
2024-06-27 15:06:15 +0530

    Add logic

--- src/components/mainBody/chat/Chat.jsx
+++ src/components/mainBody/chat/Chat.jsx
@@ -1,48 +1,54 @@
+import { useState } from 'react';
 import './chat.css';
 
 const Chat = () => {
+
+    const [user, setUser] = useState('Demo_Name');
+    const [message, setMessage] = useState('');
+    const sendMessage = () => {
+        const c = [...chats];
+        c.push({ user, message: message });
+        setChats(c);
+        setMessage('');
+    }
+    const keyDown =(event)=>{
+        if(event.key ==='Enter'){
+            event.preventDefault();
+            sendMessage();
+        }
+    }
+
+    const [chats, setChats] = useState([
+        {
+            user: 'Shiwam',
+            message: 'Nigga',
+        },
+        {
+            user: 'Demo_Name',
+            message: 'Hello',
+        }
+    ]);
+
     return (
         <div className="container">
             <div className="chat">
-                <div className="message own">
-                    <img src="./boy.png" alt="" />
-                    <div className="texts">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        <span>1 min ago</span>
+
+
+                {chats.map(c =>
+                    <div className={`message ${c.user === user ? 'own' : ''}`}>
+                        <img src="./boy.png" alt="" />
+                        <div className="texts">
+                            <h4>{c.user}</h4>
+                            <p>{c.message}</p>
+                            <span>1 min ago</span>
+                        </div>
                     </div>
-                </div>
-                <div className="message">
-                    <img src="./boy.png" alt="" />
-                    <div className="texts">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        <span>1 min ago</span>
-                    </div>
-                </div>
-                <div className="message own">
-                    <img src="./boy.png" alt="" />
-                    <div className="texts">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        <span>1 min ago</span>
-                    </div>
-                </div>
-                <div className="message">
-                    <img src="./boy.png" alt="" />
-                    <div className="texts">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        <span>1 min ago</span>
-                    </div>
-                </div>
-                <div className="message own">
-                    <img src="./boy.png" alt="" />
-                    <div className="texts">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        <span>1 min ago</span>
-                    </div>
-                </div>
+                )}
+
             </div>
             <div className="bottom">
-                <input type="text" placeholder='Send a message to your nigga...' />
-                <button className='sendButton'>Send</button>
+                <input type="text" placeholder='Send a message to your nigga...' onInput={e => setMessage(e.target.value)} onKeyDown={keyDown} value={message} />
+                <button className='sendButton' onClick={e => sendMessage()}>Send</button>
             </div>
         </div>
     )
