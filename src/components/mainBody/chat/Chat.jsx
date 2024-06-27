e0bc55d4d8ff77f8285b784c8b0e3afb5665991c
snyder-io <tushar.whitecollar@gmail.com>
2024-06-27 13:38:12 +0530

    UI SetUp

--- src/components/mainBody/chat/Chat.jsx
+++ src/components/mainBody/chat/Chat.jsx
@@ -4,7 +4,41 @@ const Chat = () => {
     return (
         <div className="container">
             <div className="chat">
-
+                <div className="message own">
+                    <img src="./boy.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./boy.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <img src="./boy.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./boy.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <img src="./boy.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
             </div>
             <div className="bottom">
                 <input type="text" placeholder='Send a message to your nigga...' />
